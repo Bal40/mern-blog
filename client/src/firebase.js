304f11af6640ff360745628f2776df3fe8bf9159
Bal40 <102305523+Bal40@users.noreply.github.com>
2024-04-03 16:41:25 +0530

    add Oauth Functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  authDomain: "mern-blog-44284.firebaseapp.com",
+  projectId: "mern-blog-44284",
+  storageBucket: "mern-blog-44284.appspot.com",
+  messagingSenderId: "842573299740",
+  appId: "1:842573299740:web:04f3f5e5cb9c0ef7245fd9"
 };
 
 // Initialize Firebase
